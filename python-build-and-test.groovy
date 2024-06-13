bba66270c6e4dcd37527a6742234a6abd50331ca
Neix20 <txen2000@gmail.com>
2024-06-14 07:13:38 +0800

    feat: Update Python Test

--- python-build-and-test.groovy
+++ python-build-and-test.groovy
@@ -1,9 +1,24 @@
+namespace=null
+
+def data = [ "apple","orange" ]
+
+
+data.each {val-> 
+    println(val)
+}
+
 pipeline {
     agent any
     
     // triggers {
-    //     cron("H/5 * * * *")
+    //     cron("2 * * * *")
     // }
+
+    parameters {
+        choice choices: ['hk', 'hkbea', 'ph', 'th'], name: "country"
+        choice choices: ['dev', 'sit', 'uat', 'prod', 'dr'], name: "environment"
+    }
+
 
     stages {
         stage('Checkout') {
@@ -21,18 +36,52 @@ pipeline {
         
         stage('Test') {
             steps {
-                git branch: 'main', url: 'https://github.com/Neix20/pytest-intro-vs.git'
-                sh 'python3 -m pytest'
+                script {
+                    git branch: 'main', url: 'https://github.com/Neix20/pytest-intro-vs.git'
+                    sh 'python3 -m pytest'
+
+                    countrySel="${country.toLowerCase()}"
+                    envSel=""
+
+                    if (environment == "dev") {
+                        envSel = "d"
+                    } else if (environment == "sit") {
+                        envSel = "s"
+                    } else if (environment == "uat") {
+                        envSel = "u"
+                    } else if (environment == "prod") {
+                        envSel = "p"
+                    } else if (environment == "dr") {
+                        envSel = "r"
+                    }
+
+                    namespace = "ns-go-${envSel}-${countrySel}-ms01"
+
+                    for (def val: )
+                }
             }
         }
         
-        stage("Check Status") {
-            steps {
-                sh 'curl --location \'https://reqres.in/api/users?page=2\' > momma.json'
+        // stage("Check Status") {
+        //     steps {
 
-                // Upload to Nexus
-                sh 'curl --fail -u admin:root --upload-file ./momma.json \'http://nexus:8081/repository/trash-upload/\''
-            }
-        }
+        //         script {
+        //             def DATE_TIME = sh(script: "date +%Y%m%d", returnStdout: true).trim()
+        //             def RANDOM_NUM = new Random().nextInt(100000)
+
+        //             def FILE_NAME = "${namespace}_${DATE_TIME}_${RANDOM_NUM}.json"
+
+        //             sh label: "Create File", script: "echo '> curl --location \'https://reqres.in/api/users?page=2\'' > ${FILE_NAME}"                
+
+        //             sh label: "Write To File", script: "curl --location 'https://reqres.in/api/users?page=2' >> ${FILE_NAME}"
+
+        //             // Upload to Nexus
+        //             sh label: "Upload File", script: "curl --fail -u admin:root --upload-file ./${FILE_NAME} 'http://nexus:8081/repository/trash-upload/'"
+
+        //             // Clean File
+        //             sh label: "Clean File", script: "rm -f ./${FILE_NAME}.json"
+        //         }
+        //     }
+        // }
     }
 }
